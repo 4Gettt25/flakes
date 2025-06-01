d0a7a55294d46eba25105a9c594493c50ad017cb
4Gettt25 <felix.guenther24@gmail.com>
2025-06-01 22:31:56 +0200

    add Python development environment flake configuration

---
+++ python/flake.nix
@@ -0,0 +1,28 @@
+# flakes/python/flake.nix
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python39
+            pkgs.poetry
+            pkgs.git
+            # add more Python-related packages here
+          ];
+          shellHook = ''
+            export NIX_CONFIG="experimental-features = nix-command flakes"
+            echo "Entering Python flake devShell"
+          '';
+        };
+      });
+}
